bfb12a1b862cb0730178c1b78a1670bf158dcedd
Perkles <eduardobnhr@gmail.com>
2022-11-28 20:12:12 -0400

    start project

---
+++ app/src/main/java/com/cursoandroid/passandodadosactivities/MainActivity.java
@@ -0,0 +1,33 @@
+ package com.cursoandroid.passandodadosactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+ public class MainActivity extends AppCompatActivity {
+
+    private Button buttonEnviar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonEnviar = findViewById(R.id.buttonEnviar);
+
+        buttonEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SegundaActivity.class);
+                //passar dados
+                intent.putExtra("nome", "eduardo");
+                intent.putExtra("idade", "22");
+                startActivity(intent);
+            }
+        });
+
+    }
+}
